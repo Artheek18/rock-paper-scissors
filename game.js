d79e28c7f5c5f3ad3e377badae6e95c2b6facd0c
Artheek18 <56484153+Artheek18@users.noreply.github.com>
2024-12-24 09:11:19 -0500

    created logic for human choice

--- game.js
+++ game.js
@@ -13,6 +13,23 @@ function getComputerChoice(){
         console.log("scissors")
         return " scissors"
     }
-
-
 }
+
+function getHumanChoice() {
+    let choice;
+
+    while (true) {
+        choice = prompt("What are you picking: rock, paper, or scissors? ").toLowerCase();
+
+        if (choice === "rock" || choice === "paper" || choice === "scissors") {
+            console.log(choice);
+            return choice;
+        } else {
+            alert("Invalid choice! Please pick rock, paper, or scissors.");
+        }
+    }
+}
+
+
+//getComputerChoice();
+getHumanChoice();
